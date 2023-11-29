6578dca16ef29e4aebb536c90a0f6e209c24adc2
Jorge <jormencar@yahoo.com>
2023-11-29 10:19:54 +0000

    add sibar for mobile - in progress

--- app/(dashboard)/_components/Navbar.tsx
+++ app/(dashboard)/_components/Navbar.tsx
@@ -1,23 +1,54 @@
-import Link from 'next/link'
+'use client'
 import { SearchBox } from './SearchBox'
 import { type User } from '@supabase/supabase-js'
 import { LogoutButton } from './logout-button'
+import { useState } from 'react'
+import Link from 'next/link'
+import { menu } from './DesktopSidebar'
+import { SubMenu } from './SubMenu'
 
 export function Navbar({ user }: { user: User }) {
+	const [sidebarOpen, setSidebarOpen] = useState(false)
 	return (
 		<>
 			<header className='flex h-14 lg:h-[60px] items-center gap-4 border-b bg-zinc-100/40 px-6 dark:bg-zinc-800/40'>
-				{/* TODO: this is the burgerMenu - need to have it working for mobile */}
-				<Link className='lg:hidden' href='#'>
+				<button
+					onClick={() => {
+						setSidebarOpen(!sidebarOpen)
+					}}
+					className='lg:hidden'
+				>
 					<IconMenu className='h-6 w-6' />
-				</Link>
+				</button>
 				<SearchBox />
 				<div className='ml-auto flex items-center gap-4'>
 					<div className='hidden lg:block'>{user.email}</div>
 					<LogoutButton />
 				</div>
 			</header>
+			{sidebarOpen && <SidebarMobile />}
 		</>
+	)
+}
+
+const SidebarMobile = () => {
+	return (
+		<nav className='grid items-start px-4 text-sm font-medium'>
+			{menu.map((item, index) =>
+				item.subSections ? (
+					<SubMenu key={index} item={item} />
+				) : (
+					<Link
+						key={index}
+						className='flex items-center gap-3 rounded-lg px-3 py-2 text-zinc-900 transition-all hover:text-zinc-900 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:text-zinc-50'
+						href={item.href}
+					>
+						{item.icon}
+						{item.text}
+					</Link>
+				)
+			)}
+		</nav>
 	)
 }
 
