15a28c7775ae3842bf110f4fdd84798ed5092c96
Jorge Mena <jormencar@yahoo.com>
2023-11-30 22:55:53 +0000

    Update fetchEmpresas.ts

--- services/data/actions/server/fetchEmpresas.ts
+++ services/data/actions/server/fetchEmpresas.ts
@@ -2,7 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
 export const fetchEmpresas = async () => {
-	const supabase = createServerComponentClient({ cookies })
+	const supabase = createServerComponentClient<Database>({ cookies })
 
 	const { data } = await supabase.from('empresas').select()
 
