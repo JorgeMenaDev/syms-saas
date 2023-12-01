c1df07dfe7698179858d5ab0199bd241a1685646
Jorge Mena <jormencar@yahoo.com>
2023-12-01 15:50:46 +0000

    Create SelectGeneric.tsx

---
+++ components/SelectGeneric.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { SelectContent } from '@radix-ui/react-select'
+import { Select, SelectGroup, SelectItem, SelectTrigger, SelectValue } from './ui/select'
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './ui/form'
+import Link from 'next/link'
+
+const ciudadesOptions = [
+	{ value: '1', label: 'Santiago' },
+	{ value: '2', label: 'Valparaiso' },
+	{ value: '3', label: 'Concepcion' }
+]
+
+export function SelectGeneric({ form }) {
+	return (
+		<FormField
+			control={form.control}
+			name='ciudad'
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>Ciudad</FormLabel>
+					<Select onValueChange={field.onChange} defaultValue={field.value}>
+						<FormControl>
+							<SelectTrigger>
+								<SelectValue placeholder='Select a verified email to display' />
+							</SelectTrigger>
+						</FormControl>
+						<SelectContent>
+							{ciudadesOptions.map((option, index) => (
+								<SelectItem key={index} value={option.value}>
+									{option.label}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+					<FormDescription>
+						You can manage email addresses in your <Link href='/examples/forms'>email settings</Link>.
+					</FormDescription>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	)
+}
