cca8fd520b59d5fbd4408383e85b6041b24bd411
Jorge Mena <jormencar@yahoo.com>
2023-12-01 15:51:13 +0000

    Create fetchEmpresaById.ts

---
+++ services/data/actions/server/empresas/fetchEmpresaById.ts
@@ -0,0 +1,13 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+// const { data } = await supabase.from('tweets').select('*, author: profiles(*), likes(user_id)')
+export const fetchEmpresaById = async (id: string) => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+
+	const { data } = await supabase.from('empresas').select().eq('id', id)
+
+	return {
+		empresa: data
+	}
+}
