9576dd142c8c39691980e0341863650c7a7dc816
Jorge Mena <jormencar@yahoo.com>
2023-12-03 16:04:25 +0000

    Create empresa.ts

---
+++ types/empresa.ts
@@ -0,0 +1,14 @@
+export interface Empresa {
+	id: string
+	rut: string
+	nombre: string
+	industria: string
+	ciiu: number
+	representanteLegal: string
+	email: string
+	telefono: string
+	ubicacion: string
+	region: string
+	ciudad: string
+	estado: string
+}
