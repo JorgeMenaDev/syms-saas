14b7e1cd6706580a6e97744aa26b640021981b1b
Jorge Mena <jormencar@yahoo.com>
2023-12-03 16:04:17 +0000

    Create ReadOnlyTableEntryForm.tsx

---
+++ components/ReadOnlyTableEntryForm.tsx
@@ -0,0 +1,55 @@
+'use client'
+import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import type * as z from 'zod'
+
+import { Form, FormField, FormControl, FormDescription, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { type ConfigParameter } from './TableEntryForm'
+
+export interface ReadOnlyTableEntryFormProps {
+	tableSchema: z.ZodObject<any, any, any>
+	configParameters: ConfigParameter[]
+	initialValues: Record<string, any> // Initial values for pre-filling the form
+}
+
+export const ReadOnlyTableEntryForm: React.FC<ReadOnlyTableEntryFormProps> = ({
+	tableSchema,
+	configParameters,
+	initialValues
+}) => {
+	const form = useForm<any>({
+		resolver: zodResolver(tableSchema),
+		defaultValues: initialValues, // Set default values for the form fields
+		shouldUnregister: false // Keep fields registered for read-only form
+	})
+
+	return (
+		<Form {...form}>
+			<form className='space-y-8'>
+				{configParameters.map((config, index) => (
+					<FormField
+						key={index}
+						control={form.control}
+						name={config.name}
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>{config.label}</FormLabel>
+
+								{config.type === 'input' && (
+									<FormControl>
+										<Input placeholder={config.placeholder} readOnly value={initialValues[config.name]} />
+									</FormControl>
+								)}
+
+								<FormDescription>{config.description}</FormDescription>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				))}
+			</form>
+		</Form>
+	)
+}
