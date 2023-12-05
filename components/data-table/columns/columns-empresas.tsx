6d6e1a95d8dcde11f38d6a150a38b9da2853f19a
Jorge Mena <jormencar@yahoo.com>
2023-12-05 16:19:45 +0000

    Update columns-empresas.tsx

--- components/data-table/columns/columns-empresas.tsx
+++ components/data-table/columns/columns-empresas.tsx
@@ -6,31 +6,6 @@ import { DataTableColumnHeader } from '../data-table-column-header'
 import { DataTableRowActions } from '../data-table-row-actions'
 
 export const empresasColumns: Array<ColumnDef<any>> = [
-	// {
-	// 	id: 'select',
-	// 	header: ({ table }) => (
-	// 		<Checkbox
-	// 			checked={table.getIsAllPageRowsSelected()}
-	// 			onCheckedChange={value => {
-	// 				table.toggleAllPageRowsSelected(!!value)
-	// 			}}
-	// 			aria-label='Select all'
-	// 			className='translate-y-[2px]'
-	// 		/>
-	// 	),
-	// 	cell: ({ row }) => (
-	// 		<Checkbox
-	// 			checked={row.getIsSelected()}
-	// 			onCheckedChange={value => {
-	// 				row.toggleSelected(!!value)
-	// 			}}
-	// 			aria-label='Select row'
-	// 			className='translate-y-[2px]'
-	// 		/>
-	// 	),
-	// 	enableSorting: false,
-	// 	enableHiding: false
-	// },
 	{
 		accessorKey: 'nombre',
 		header: ({ column }) => <DataTableColumnHeader column={column} title='Nombre' />,
