b0ca3cea32d66c07bf6aac498d711bc2d737cf2e
Jorge Mena <jormencar@yahoo.com>
2023-12-05 07:34:12 +0000

    Update fetchEmpresaById.ts

--- services/data/actions/server/empresas/fetchEmpresaById.ts
+++ services/data/actions/server/empresas/fetchEmpresaById.ts
@@ -1,7 +1,7 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
-export const fetchEmpresaById = async (id: string): Promise<{ empresa: any | null }> => {
+export const fetchEmpresaById = async (id: string): Promise<any | null> => {
 	const supabase = createServerComponentClient<Database>({ cookies })
 	console.log({ id })
 	const { data } = await supabase
@@ -18,9 +18,7 @@ export const fetchEmpresaById = async (id: string): Promise<{ empresa: any | nul
 	console.log({ data })
 
 	if (!data || data.length === 0 || data?.[0] === null) {
-		return {
-			empresa: null
-		}
+		return null
 	}
 
 	const empresaDetails = data?.[0]
@@ -44,5 +42,5 @@ export const fetchEmpresaById = async (id: string): Promise<{ empresa: any | nul
 
 	console.log({ empresa })
 
-	return { empresa }
+	return empresa
 }
