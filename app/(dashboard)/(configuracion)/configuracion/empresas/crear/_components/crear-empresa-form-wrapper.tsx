4ba0a104602ff928433d4202899f1fe19f4ea13a
Jorge Mena <jormencar@yahoo.com>
2023-12-06 14:18:54 +0000

    Create crear-empresa-form-wrapper.tsx

---
+++ app/(dashboard)/(configuracion)/configuracion/empresas/crear/_components/crear-empresa-form-wrapper.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { TableEntryForm } from '@/components/TableEntryForm'
+import { createEmpresa } from '@/services/data/actions/server/empresas/post-empresa'
+import { composeEmpresaConfigParameters, createEmpresaFormSchema } from '@/services/data/actions/server/empresas/utils'
+import { toast } from 'sonner'
+import { type z } from 'zod'
+
+const formSchema = createEmpresaFormSchema()
+
+export function CrearEmpresaFormWrapper({
+	ciudadesOptions,
+	regionesOptions,
+	ciiusOptions
+}: {
+	ciudadesOptions: Array<{ value: string; label: string }>
+	regionesOptions: Array<{ value: string; label: string }>
+	ciiusOptions: Array<{ value: string; label: string }>
+}) {
+	async function onSubmit(values: z.infer<typeof formSchema>) {
+		try {
+			const { error } = await createEmpresa(values)
+
+			if (error) {
+				toast.error(`Hubo un error al crear la empresa: ${error}`)
+			} else {
+				toast.success('Empresa creada correctamente.')
+				return true
+			}
+		} catch (error) {
+			console.error({ error })
+			toast.error('Hubo un error al crear la empresa, si el error persiste contacte a soporte.')
+		}
+	}
+
+	const configParameters = composeEmpresaConfigParameters({
+		ciudadesOptions,
+		regionesOptions,
+		ciiusOptions
+	})
+
+	// initial values are all empty strings
+	const initialValues = Object.fromEntries(configParameters.map(config => [config.name, '']))
+
+	return (
+		<TableEntryForm
+			initialValues={initialValues}
+			tableSchema={formSchema}
+			configParameters={configParameters}
+			onSubmit={onSubmit}
+		/>
+	)
+}
