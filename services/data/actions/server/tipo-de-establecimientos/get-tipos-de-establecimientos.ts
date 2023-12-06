16eec63b4f63433ac5d925a7f08fa3bc64c05a4e
Jorge Mena <jormencar@yahoo.com>
2023-12-06 14:19:37 +0000

    Create get-tipos-de-establecimientos.ts

---
+++ services/data/actions/server/tipo-de-establecimientos/get-tipos-de-establecimientos.ts
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const fetchTiposDeEstablecimientos = async () => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+
+	const { data: ciius } = await supabase.from('tipoEstablecimientos').select('*')
+
+	if (!ciius || ciius.length === 0) return null
+
+	return ciius
+}
+/**
+ * 	Function de utilidad, para obtener las ciius en formato para select/dropdowns
+ */
+export const fetchTiposDeEstablecimientosForSelect = async () => {
+	const tiposDeEstablecimientos = await fetchTiposDeEstablecimientos()
+	if (!tiposDeEstablecimientos) return null
+
+	console.log({ tiposDeEstablecimientos })
+
+	const tiposDeEstablecimientosForSelect = tiposDeEstablecimientos.map(ciiu => ({ value: ciiu.id, label: ciiu.nombre }))
+
+	return tiposDeEstablecimientosForSelect
+}
