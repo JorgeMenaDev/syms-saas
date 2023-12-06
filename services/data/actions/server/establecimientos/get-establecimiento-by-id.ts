2c458d36be0f5f21b5b77af8010d718abe44750f
Jorge Mena <jormencar@yahoo.com>
2023-12-06 14:19:26 +0000

    Create get-establecimiento-by-id.ts

---
+++ services/data/actions/server/establecimientos/get-establecimiento-by-id.ts
@@ -0,0 +1,45 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const fechEstablecimientoById = async (id: string) => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+	console.log({ id })
+	const { data, error } = await supabase
+		.from('establecimientos')
+		.select(
+			`*,
+      region: region_id(nombre),
+      ciudad: ciudad_id(nombre),
+      tipo_establecimiento: tipo_establecimiento_id(nombre)
+    `
+		)
+		.eq('id', id)
+
+	console.log({ data, error })
+
+	if (!data || data.length === 0 || data?.[0] === null) {
+		return null
+	}
+
+	const establecimientoDetails = data?.[0]
+
+	console.log({ establecimientoDetails })
+
+	const establecimiento = {
+		id: establecimientoDetails.id,
+		nombre: establecimientoDetails.nombre,
+		correo: establecimientoDetails.correo,
+		telefono: establecimientoDetails.telefono,
+		direccion: establecimientoDetails.direccion,
+		region: establecimientoDetails.region_id.toString(),
+		ciudad: establecimientoDetails.ciudad_id.toString(),
+		encargado: establecimientoDetails.encargado,
+		nopel: establecimientoDetails.nopel,
+		respel: establecimientoDetails.respel,
+		id_vu: establecimientoDetails.id_vu,
+		usuarios: establecimientoDetails.usuarios,
+		tipo: establecimientoDetails.tipo_establecimiento_id
+	}
+
+	return establecimiento
+}
