925c52af30cce8c9ade522330191938e348d9c98
Jorge Mena <jormencar@yahoo.com>
2023-12-07 19:18:47 +0000

    Update TableEntryForm.tsx

--- components/TableEntryForm.tsx
+++ components/TableEntryForm.tsx
@@ -16,7 +16,7 @@ export interface ConfigParameter {
 	label: string
 	type: 'input' | 'select' | 'textarea' // Specify the type of the field
 	inputType?: 'text' | 'number' | 'email' | 'password' // Specify the input type for input type
-	options?: Array<{ value: string; label: string }> | undefined // Options for select type
+	options?: Array<{ value: string; label: string; idRegion?: string }> | undefined // Options for select type
 	placeholder?: string
 	description?: string
 }
@@ -34,11 +34,16 @@ export const TableEntryForm: React.FC<DynamicTableEntryFormProps> = ({
 	configParameters,
 	onSubmit
 }) => {
+	const cityOptions = configParameters.find(config => config.name === 'ciudad')?.options ?? []
+	const [loading, setLoading] = React.useState(false)
+	const [selectedRegion, setSelectedRegion] = React.useState<string | null>(null)
+	const [selectedCity, setSelectedCity] = React.useState<string | null>(null)
+	const [citiesOptions, setCitiesOptions] = React.useState(cityOptions)
+
 	const form = useForm<any>({
 		resolver: zodResolver(tableSchema),
 		defaultValues: initialValues
 	})
-	const [loading, setLoading] = React.useState(false)
 
 	function handleSubmit(values: any) {
 		setLoading(true)
@@ -55,11 +60,35 @@ export const TableEntryForm: React.FC<DynamicTableEntryFormProps> = ({
 			})
 	}
 
+	const configParametersWithRegionAndCity = configParameters.filter(
+		config => config.name === 'region' || config.name === 'ciudad'
+	)
+
+	const configParametersWithoutRegionAndCity = configParameters.filter(
+		config => config.name !== 'region' && config.name !== 'ciudad'
+	)
+
+	React.useEffect(() => {
+		// if user select a city first, then a region, we want to keep the city selected only if it belongs to the region
+		const city = cityOptions.find(option => option.value === selectedCity)
+		const cityBelongsToRegion = city?.idRegion === selectedRegion
+
+		if (!cityBelongsToRegion) {
+			form.setValue('ciudad', '')
+		}
+
+		if (cityOptions && selectedRegion !== null) {
+			const cityOptionsForRegion = cityOptions.filter(option => option.idRegion === selectedRegion)
+
+			setCitiesOptions(cityOptionsForRegion)
+		}
+	}, [selectedRegion])
+
 	return (
 		<Card className='w-full border-0 lg:p-5'>
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-8'>
-					{configParameters.map((config, index) => (
+					{configParametersWithoutRegionAndCity.map((config, index) => (
 						<FormField
 							key={index}
 							control={form.control}
@@ -76,7 +105,7 @@ export const TableEntryForm: React.FC<DynamicTableEntryFormProps> = ({
 									)}
 
 									{/* selects */}
-									{config.type === 'select' && (
+									{config.type === 'select' && config.name !== 'region' && config.name !== 'ciudad' && (
 										<Select onValueChange={field.onChange} value={field.value}>
 											<FormControl>
 												<SelectTrigger>
@@ -93,10 +122,82 @@ export const TableEntryForm: React.FC<DynamicTableEntryFormProps> = ({
 										</Select>
 									)}
 
+									{/* ********************* REGION AND CITIES */}
+
 									{/* Add support for other types like textarea if needed */}
 									<FormDescription>{config.description}</FormDescription>
 									<FormMessage />
 								</FormItem>
+							)}
+						/>
+					))}
+
+					{configParametersWithRegionAndCity.map((config, index) => (
+						<FormField
+							key={index}
+							control={form.control}
+							name={config.name}
+							render={({ field }) => (
+								<>
+									{config.name === 'ciudad' && (
+										<FormItem>
+											<FormLabel>{config.label}</FormLabel>
+											<Select
+												onValueChange={value => {
+													setSelectedCity(value)
+													field.onChange(value)
+												}}
+												value={field.value}
+											>
+												<FormControl>
+													<SelectTrigger>
+														<SelectValue placeholder={`Selecciona ${config.placeholder}`} />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent>
+													{citiesOptions.map((option, optionIndex) => (
+														<SelectItem key={optionIndex} value={option.value}>
+															{option.label}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+
+											<FormDescription>{config.description}</FormDescription>
+											<FormMessage />
+										</FormItem>
+									)}
+
+									{config.name === 'region' && (
+										<FormItem>
+											<FormLabel>{config.label}</FormLabel>
+
+											<Select
+												onValueChange={value => {
+													setSelectedRegion(value)
+													field.onChange(value)
+												}}
+												value={field.value}
+											>
+												<FormControl>
+													<SelectTrigger>
+														<SelectValue placeholder={`Selecciona ${config.placeholder}`} />
+													</SelectTrigger>
+												</FormControl>
+												<SelectContent>
+													{config.options?.map((option, optionIndex) => (
+														<SelectItem key={optionIndex} value={option.value}>
+															{option.label}
+														</SelectItem>
+													))}
+												</SelectContent>
+											</Select>
+
+											<FormDescription>{config.description}</FormDescription>
+											<FormMessage />
+										</FormItem>
+									)}
+								</>
 							)}
 						/>
 					))}
