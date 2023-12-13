52f64e3b3b42bc66b1afd11ae3aaeb0a89d44625
Jorge Mena <jormencar@yahoo.com>
2023-12-13 22:45:25 +0000

    Add authentication check and redirect to dashboard if user is logged in

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,19 @@
 import { SiteHeaderWithNoAuth } from '@/components/SiteHeaderWithNoAuth'
 import { HomePageHero } from './_components/Hero'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function MainPageHome() {
+export default async function MainPageHome() {
+	const supabase = createServerComponentClient({ cookies })
+	const {
+		data: { session }
+	} = await supabase.auth.getSession()
+
+	if (session?.user) {
+		redirect('/dashboard')
+	}
+
 	return (
 		<>
 			<SiteHeaderWithNoAuth />
