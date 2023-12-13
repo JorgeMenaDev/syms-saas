cfc318ae5902af57c14b2a3b1ecd766d915cf6bd
Jorge Mena <jormencar@yahoo.com>
2023-12-13 22:45:52 +0000

    Add fetchUsuarios function to get usuarios from the server

---
+++ services/data/actions/server/usuarios/get-usuarios.ts
@@ -0,0 +1,43 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const fetchUsuarios = async () => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+
+	const { data, error } = await supabase.from('usuarios').select(
+		`*,
+      tipo: tipo_de_usuario_id(nombre_tipo)
+		`
+	)
+
+	console.log({ data, error })
+
+	if (error) {
+		console.error(error)
+		return null
+	}
+
+	if (!data || data.length === 0) {
+		return null
+	}
+
+	const formattedData = data.map(item => {
+		console.log({ item })
+		return {
+			id: item.id,
+			nombre: item.nombre,
+			correo: item.email,
+			telefono: item.telefono,
+			tipo: item.tipo.nombre_tipo,
+			empresas: item.empresas ?? 0,
+			establecimientos: item.establecimientos ?? 0,
+			usuario: item.usuario,
+			accion: item.accion,
+			activo: item.activo
+		}
+	})
+
+	console.log({ formattedData })
+
+	return formattedData
+}
