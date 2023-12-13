47cab4947ed2deb07281c8b0ac10d986be2d7639
Jorge Mena <jormencar@yahoo.com>
2023-12-13 22:45:59 +0000

    Add usuariosColumns array to utils.ts

---
+++ services/data/actions/server/usuarios/utils.ts
@@ -0,0 +1,12 @@
+// this is really important, because is the order we have to follow.
+// columns-wrapper follow this order.
+export const usuariosColumns = [
+	'Nombre',
+	'Correo',
+	'Telefono',
+	'Tipo de Usuario',
+	'Empresas',
+	'Establecimientos',
+	'Acciòn',
+	'Usuario'
+]
