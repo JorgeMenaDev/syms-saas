1afac8dee5ac7fe91c855974b82f717db005e468
Jorge Mena <jormencar@yahoo.com>
2023-12-14 23:02:02 +0000

    Add transportistasColumns to utils.ts

---
+++ services/data/actions/server/transportistas/utils.ts
@@ -0,0 +1,13 @@
+// this is really important, because is the order we have to follow.
+// columns-wrapper follow this order.
+export const transportistasColumns = [
+	'Razon Social',
+	'Rut',
+	'Direccion',
+	'Ciudad',
+	'Region',
+	'Representante Legal',
+	'Estado',
+	'Camiones',
+	'Autorizacion'
+]
