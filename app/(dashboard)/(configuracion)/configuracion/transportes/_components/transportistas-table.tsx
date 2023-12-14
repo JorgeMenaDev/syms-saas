a654f2cdf7cc2250c22ecfdef8178afe8292d23d
Jorge Mena <jormencar@yahoo.com>
2023-12-14 23:00:54 +0000

    Add TransportistasTable component

---
+++ app/(dashboard)/(configuracion)/configuracion/transportes/_components/transportistas-table.tsx
@@ -0,0 +1,26 @@
+import { NoDataAvailableMessage } from '@/components/NoDataAvailableMessage'
+
+import ColumnsWrapper from './columns-wrapper'
+import { fetchTransportistas } from '@/services/data/actions/server/transportistas/get-transportistas'
+
+export default async function TransportistasTable() {
+	const transportistas = await fetchTransportistas()
+
+	console.log(transportistas)
+
+	if (transportistas === null) {
+		return (
+			<div className='flex items-center justify-center h-full'>
+				<div className='flex flex-col items-center space-y-4'>
+					<NoDataAvailableMessage />
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div className='relative py-14 pr-11'>
+			<ColumnsWrapper transportistas={transportistas} />
+		</div>
+	)
+}
