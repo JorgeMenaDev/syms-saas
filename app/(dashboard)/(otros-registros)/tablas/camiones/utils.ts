1df156a338ca533e4f76a469ebb565ed48ece515
Jorge Mena <jormencar@yahoo.com>
2023-12-15 13:50:30 +0000

    Add camionesColumns array to utils.ts

---
+++ app/(dashboard)/(otros-registros)/tablas/camiones/utils.ts
@@ -0,0 +1,21 @@
+// this is really important, because is the order we have to follow.
+// columns-wrapper follow this order.
+export const camionesColumns = [
+	'Resolucion',
+	'Año Resolucion',
+	'Autoridad',
+	'Rut',
+	'Representante Legal',
+	'Correo',
+	'Telefono',
+	'Camion',
+	'Modelo',
+	'Año Camion',
+	'Patente',
+	'Capacidad',
+	'Vigencia',
+	'Estado',
+	'Autoriza',
+	'Comentario',
+	'Documento'
+]
