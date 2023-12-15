8f7e1b2bab7680b0d2e156fd1d0be0570ff474f6
Jorge Mena <jormencar@yahoo.com>
2023-12-15 22:04:14 +0000

    Add fetchTransportistaById function to get transportista by ID

---
+++ services/data/actions/server/transportistas/get-transportista-by-id.ts
@@ -0,0 +1,60 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+interface join {
+	ciudad: any
+	region: any
+}
+
+type Transportista = Database['public']['Tables']['transportistas']['Row']
+
+export const fetchTransportistaById = async (id: string): Promise<any | null> => {
+	const supabase = createServerComponentClient<Database>({ cookies })
+
+	const { data } = await supabase
+		.from('transportistas')
+		.select(
+			`*,
+			region: region_id(nombre),
+			ciudad: ciudad_id(nombre)
+		`
+		)
+		.eq('id', id)
+
+	if (!data || data.length === 0 || data?.[0] === null) {
+		return null
+	}
+
+	const transportista = transformTransportista(data[0])
+	console.log({ transportista })
+
+	return transportista
+}
+
+function transformTransportista(transportista: Transportista) {
+	const {
+		id,
+		autorizacion,
+		camiones,
+		direccion,
+		estado,
+		representante_legal,
+		razon_social,
+		rut,
+		ciudad_id,
+		region_id
+	} = transportista
+
+	return {
+		id,
+		representanteLegal: representante_legal,
+		autorizacion,
+		camiones,
+		ciudad: ciudad_id.toString(),
+		direccion,
+		estado,
+		region: region_id.toString(),
+		razonSocial: razon_social,
+		rut
+	}
+}
