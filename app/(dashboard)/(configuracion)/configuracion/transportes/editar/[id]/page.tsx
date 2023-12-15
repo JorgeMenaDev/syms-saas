48be71a7416c49538c32be2bacf75e0663897081
Jorge Mena <jormencar@yahoo.com>
2023-12-15 22:04:07 +0000

    Add EditarTransportistaPage component

---
+++ app/(dashboard)/(configuracion)/configuracion/transportes/editar/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { EditarTransportistaForm } from './_components/editar-transportista-form'
+import { fetchCiudadesForSelect } from '@/services/data/actions/server/ciudades/fetchCiudades'
+import { fetchRegionesForSelect } from '@/services/data/actions/server/regiones/fetchRegiones'
+import { fetchTransportistaById } from '@/services/data/actions/server/transportistas/get-transportista-by-id'
+
+export default async function EditarTransportistaPage({ params }: { params: { id: string } }) {
+	console.log({ id: params?.id })
+	const [transportista, ciudades, regiones] = await Promise.all([
+		fetchTransportistaById(params?.id),
+		fetchCiudadesForSelect(),
+		fetchRegionesForSelect()
+	])
+
+	// This is very unlikely to happen, but to keep ts happy
+	if (transportista === null || ciudades === null || regiones === null) {
+		return (
+			<p>
+				Hubo un error al cargar los datos del transportista, por favor intente de nuevo, si el error persiste contacte a
+				soporte.
+			</p>
+		)
+	}
+
+	return <EditarTransportistaForm transportista={transportista} ciudadesOptions={ciudades} regionesOptions={regiones} />
+}
