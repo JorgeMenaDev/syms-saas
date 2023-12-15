ee4ef6cd0f5379d7f67424b2d41d6119fc006553
Jorge Mena <jormencar@yahoo.com>
2023-12-15 14:14:07 +0000

    Refactor transportista columns and remove unused code

--- app/(dashboard)/(configuracion)/configuracion/transportes/_components/columns-wrapper.tsx
+++ app/(dashboard)/(configuracion)/configuracion/transportes/_components/columns-wrapper.tsx
@@ -14,9 +14,6 @@ import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 
 import { delay } from '@/utils/re-usable-functions/delay'
 import { deleteTransportistaById } from '@/services/data/actions/server/transportistas/delete-transportista-by-id'
-import { TableModal } from '@/components/TableModal'
-import { CamionesFilters } from '@/app/(dashboard)/(otros-registros)/tablas/camiones/_components/camiones-filters'
-import { fetchCamionesClientByIds } from '@/services/data/actions/client/get-camiones-client'
 import { useRouter } from 'next/navigation'
 
 export default function ColumnsWrapper({ transportistas }: any) {
@@ -24,8 +21,6 @@ export default function ColumnsWrapper({ transportistas }: any) {
 	const [openDeleteModal, setOpenDeleteModal] = useState(false)
 	const [openCamionesModal, setOpenCamionesModal] = useState(false)
 	const [transportistaId, setTransportistaId] = useState('')
-	const [camiones, setCamiones] = useState<any>([])
-	const [loading, setLoading] = useState(false)
 
 	const [id, setId] = useState('')
 	const [isDeleting, setIsDeleting] = useState(false)
@@ -159,8 +154,9 @@ export default function ColumnsWrapper({ transportistas }: any) {
 									<MagnifyingGlassIcon
 										className='w-6 h-6 cursor-pointer text-primary font-bold'
 										onClick={async () => {
-											// setOpenCamionesModal(true)
-											router.push(`/tablas/camiones?id=${row.original.id}`)
+											setTransportistaId(row.original.id)
+
+											toggleCamionesModal()
 										}}
 									/>
 								</span>
@@ -209,235 +205,9 @@ export default function ColumnsWrapper({ transportistas }: any) {
 		}
 	]
 
-	const camionesColumns: Array<ColumnDef<any>> = [
-		{
-			accessorKey: 'resolucion',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Resolucion' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('resolucion')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'añoResolucion',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Año Resolucion' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('añoResolucion')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'autoridad',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Autoridad' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('autoridad')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'rut',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Rut' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('rut')}</span>
-					</div>
-				)
-			},
-			filterFn: (row, id, value) => {
-				return value.includes(row.getValue(id))
-			}
-		},
-		{
-			accessorKey: 'representanteLegal',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Representante Legal' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('representanteLegal')}</span>
-					</div>
-				)
-			},
-			filterFn: (row, id, value) => {
-				return value.includes(row.getValue(id))
-			}
-		},
-		{
-			accessorKey: 'correo',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Correo' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('correo')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'telefono',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Telefono' />,
-			cell: ({ row }) => {
-				const value = row.getValue('telefono')
-				// value is a boolean so if it's true I want to display 'Activo' and if it's false I want to display 'Inactivo'
-				return (
-					<div className='flex items-center'>
-						<span>{value ? 'Activo' : 'Inactivo'}</span>
-					</div>
-				)
-			},
-			filterFn: (row, id, value) => {
-				return value.includes(row.getValue(id))
-			}
-		},
-		{
-			accessorKey: 'camion',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Camion' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('camion')}</span>
-					</div>
-				)
-			},
-			filterFn: (row, id, value) => {
-				return value.includes(row.getValue(id))
-			}
-		},
-		{
-			accessorKey: 'modelo',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Modelo' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('modelo')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'añoCamion',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Año Camion' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('añoCamion')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'patente',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Patente' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('patente')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'capacidad',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Capacidad' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('capacidad')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'vigencia',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Vigencia' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('vigencia')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'estado',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Estado' />,
-			cell: ({ row }) => {
-				const value = row.getValue('estado')
-				// value is a boolean so if it's true I want to display 'Activo' and if it's false I want to display 'Inactivo'
-				return (
-					<div className='flex items-center'>
-						<span>{value ? 'Activo' : 'Inactivo'}</span>
-					</div>
-				)
-			},
-			filterFn: (row, id, value) => {
-				return value.includes(row.getValue(id))
-			}
-		},
-		{
-			accessorKey: 'autoriza',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Autoriza' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('autoriza')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'comentario',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Comentario' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('comentario')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			accessorKey: 'documento',
-			header: ({ column }) => <DataTableColumnHeader column={column} title='Documento' />,
-			cell: ({ row }) => {
-				return (
-					<div className='flex items-center'>
-						<span>{row.getValue('documento')}</span>
-					</div>
-				)
-			}
-		},
-		{
-			id: 'actions',
-			cell: ({ row }) => (
-				<DataTableRowActions
-					actions={[
-						{
-							label: 'Editar',
-							redirectTo: id => `/configuracion/camiones/editar/${id}`
-						},
-						{
-							label: 'Eliminar',
-							delete: id => {
-								// toggle()
-								// setId(id)
-							}
-						}
-					]}
-					row={row}
-				/>
-			)
-		}
-	]
+	function moveToCamionesPage() {
+		router.push(`/tablas/camiones?id=${transportistaId}`)
+	}
 
 	return (
 		<Fragment>
@@ -451,17 +221,16 @@ export default function ColumnsWrapper({ transportistas }: any) {
 				<span className=' font-semibold'>Se eliminarán todos los datos relacionados a este transportista.</span>
 			</GenericModal>
 
-			<TableModal
-				loading={loading}
+			<GenericModal
+				title='Ver camiones'
+				description='Ver camiones relacionados a este transportista'
 				open={openCamionesModal}
 				toggle={toggleCamionesModal}
-				title='Camiones'
-				description={`Registros de camiones para transportista ${
-					transportistas.find((transportista: any) => transportista.id === transportistaId)?.razonSocial
-				}`}
+				onConfirm={moveToCamionesPage}
+				isDisabled={isDeleting}
 			>
-				<DataTable filters={CamionesFilters} data={camiones} columns={camionesColumns} />
-			</TableModal>
+				<span className=' font-semibold'>Este link te llevará a la página de camiones con los filtros aplicados.</span>
+			</GenericModal>
 		</Fragment>
 	)
 }
