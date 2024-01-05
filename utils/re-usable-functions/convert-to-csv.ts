f913f48d43395fdc39c064a74bce304c415b3d5a
Jorge Mena <jormencar@yahoo.com>
2024-01-05 13:20:49 +0000

    Add console.log statements and map boolean values in convertToCSV function

--- utils/re-usable-functions/convert-to-csv.ts
+++ utils/re-usable-functions/convert-to-csv.ts
@@ -1,5 +1,6 @@
 export function convertToCSV(data: any) {
 	const csvData = []
+	console.log(data)
 
 	// Push header row
 	const headerRow = Object.keys(data[0])
@@ -8,9 +9,23 @@ export function convertToCSV(data: any) {
 	// Push data rows
 	// @ts-expect-error // <--
 	data.forEach(item => {
-		const row = Object.values(item)
+		// if the column is "estado" this means the value is a boolean, we need to map this to instead return "Activo" or "Inactivo"
+
+		const row = Object.values(item).map((value, index) => {
+			if (headerRow[index] === 'estado') {
+				return value ? 'Activo' : 'Inactivo'
+			}
+
+			return value
+		})
+
 		csvData.push(row)
+
+		// const row = Object.values(item)
+		// csvData.push(row)
 	})
+
+	console.log(csvData)
 
 	return csvData
 }
